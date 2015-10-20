59f7e50c51c4e0e50dadffddb7295df166b15eaf
KhalilC <klcrumpler@gmail.com>
2015-10-20 00:33:14 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+class SmallestMultiple
+  attr_accessor :input
+  def initialize(input)
+    @input = input
+  end
+
+  def divisible?(current_number)
+    (2...input).each do |multiple|
+      return false unless current_number % multiple == 0
+    end
+    return true
+  end
+
+  def lcm
+    counter = input
+    loop do
+      divisible?(counter)
+      counter += input unless divisible?(counter)
+      break if divisible?(counter)
+    end
+    counter
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def divisible?(original_number, current_number)
+  (2...original_number).each do |multiple|
+    return false unless current_number % multiple == 0
+  end
+  return true  
+end
+
+def smallest_multiple(input)
+  counter = input
+  loop do
+    divisible?(input, counter)
+    counter += input unless divisible?(input, counter)
+    break if divisible?(input, counter)
+  end
+  counter
+end
